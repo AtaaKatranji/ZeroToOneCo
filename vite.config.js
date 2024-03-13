6f1a5f5168186385cf804509e77a534f22606e23
Ataa Katranji <ataa.katranji@gmail.com>
2024-03-13 13:30:18 +0300

    deployed website

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'http://AtaaKatranji.github.io/ZeroToOneCo',
+    // Set the base URL if necessary
+    // base: '/your-app-name/',
+  }
 })
